02e5d7c9b93d85c36528b1b2648f4a3e4744298e
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-07-10 13:44:29 +0300

    initial commit

---
+++ pixcolumns.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ Plugin Name: PixColumns
+ Plugin URI: 
+ Description: Play with columns the easy way.
+ Version: 0.0.1
+ Author: Vlad Olaru, George Olaru
+ Author URI: https://pixelgrade.com
+ License: GPLv2 or later
+*/
+
+class PixColumns {
+
+	/**
+	 * Handles initializing this class and returning the singleton instance after it's been cached.
+	 *
+	 * @return null|PixColumns
+	 */
+	public static function get_instance() {
+		// Store the instance locally to avoid private static replication
+		static $instance = null;
+
+		if ( null === $instance ) {
+			$instance = new self();
+			self::_setup_plugin();
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * An empty constructor
+	 */
+	public function __construct() { /* Purposely do nothing here */ }
+
+	/**
+	 * Handles registering hooks that initialize this plugin.
+	 */
+	public static function _setup_plugin() {
+		add_filter( 'mce_external_plugins', array( __CLASS__, 'mce_external_plugins' ) );
+		add_filter( 'mce_buttons_2', array( __CLASS__, 'mce_buttons_2' ) );
+		add_filter( 'content_save_pre', array( __CLASS__, 'content_save_pre' ), 20 );
+
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'register_admin_assets' ) );
+	}
+
+	/**
+	 * Registers and enqueues admin-specific styles.
+	 */
+	public static function register_admin_assets() {
+		wp_enqueue_style( 'pixcolumns-style', plugin_dir_url( __FILE__ ) . 'assets/css/admin.css' );
+
+	} // end register_admin_assets
+
+	/**
+	 * Initialize TinyMCE table plugin and custom TinyMCE plugin
+	 *
+	 * @param array $plugin_array Array of TinyMCE plugins
+	 * @return array Array of TinyMCE plugins
+	 */
+	public static function mce_external_plugins( $plugin_array ) {
+//		global $tinymce_version;
+//		$variant = ( defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ) ? '' : '.min';
+		$variant = '';
+
+		$plugin_array['pixcolumns'] = plugin_dir_url( __FILE__ ) . 'assets/tinymce-pixcolumns/plugin' . $variant . '.js';
+
+		return $plugin_array;
+	}
+
+	/**
+	 * Add TinyMCE columns control buttons
+	 *
+	 * @param array $buttons Buttons for the second row
+	 * @return array Buttons for the second row
+	 */
+	public static function mce_buttons_2( $buttons ) {
+//		global $tinymce_version;
+
+		// in case someone is manipulating other buttons, drop columns controls at the end of the row
+		if ( ! $pos = array_search( 'undo', $buttons ) ) {
+			array_push( $buttons, 'pixcolumns' );
+			return $buttons;
+		}
+
+		$buttons = array_merge( array_slice( $buttons, 0, $pos ), array( 'pixcolumns' ), array_slice( $buttons, $pos ) );
+
+		return $buttons;
+	}
+
+	/**
+	 * Fixes weirdness resulting from wpautop and formatting clean up not built for tables
+	 *
+	 * @param string $content Editor content before WordPress massaging
+	 * @return string Editor content before WordPress massaging
+	 */
+	public static function content_save_pre( $content ) {
+		if ( false !== strpos( $content, '<table' ) ) {
+			// paragraphed content inside of a td requires first paragraph to have extra line breaks (or else autop breaks)
+			$content  = preg_replace( "/<td([^>]*)>(.+\r?\n\r?\n)/m", "<td$1>\n\n$2", $content );
+
+			// make sure there's space around the table
+			if ( substr( $content, -8 ) == '</table>' ) {
+				$content .= "\n<br />";
+			}
+		}
+		
+		return $content;
+	}
+}
+
+PixColumns::get_instance();
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== PixColumns ===
+Contributors: vladolaru
+Donate link:
+Tags: columns, grid, editor, WYSIWYG, buttons, tinymce
+Requires at least: 4.0
+Tested up to: 4.3
+Stable tag: 0.0.1
+
+Adds columns editing controls to the visual content editor (TinyMCE).
+
+== Description ==
+
+Adds columns editing controls to the visual content editor (TinyMCE) based on the table editing UX.
+
+Please note that the columns controls are contained in the “kitchen sink” toolbar, toggled with the last button on the first row of controls.
+
+== Installation ==
+
+1. Install easily with the WordPress plugin control panel or manually download the plugin and upload the folder 
+`pixcolumns` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Show the toolbar in the editor by opening the "kitchen sink" (the last button in the first row)
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 0.0.1 =
+
+Initial commit.
